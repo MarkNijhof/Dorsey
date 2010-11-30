1e2932b90415af492b6d3e92e773ae17be0a9df9
Mark Nijhof <Mark.Nijhof@gmail.com>
2010-11-30 23:20:11 +0100

    added view logic

--- lib/dorsey/server.rb
+++ lib/dorsey/server.rb
@@ -3,7 +3,6 @@ module Dorsey
     attr_reader :config
     attr_reader :articles
     
-    
     def initialize config = {}, &blk
       @config = config.is_a?(Dorsey::Config) ? config : Dorsey::Config.new(config)
       @config.instance_eval(&blk) if block_given?
